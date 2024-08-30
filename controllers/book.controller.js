0b874a83b60391951b3c1c575030572d222dba2b
thaobuihb <thaohb@gmail.com>
2024-08-30 17:02:59 +0700

    book api

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,7 +1,164 @@
-const Book = require("../models/Book");
-
+const Book = require("../models/Book.js");
+const Review = require("../models/Review.js");
+const { sendResponse, catchAsync, AppError } = require("../helpers/utils");
+const mongoose = require("mongoose");
 const bookController = {};
 
+//create book
+bookController.createBook = catchAsync(async (req, res, next) => {
+  const { name, author, price, publicationDate, img, description } = req.body;
+  if (!name || !author || !price || !publicationDate || !img || !description) {
+    throw new AppError(400, "Missing required fields", "Create Book Error");
+  }
 
+  const book = await Book.create({
+    name,
+    author,
+    price,
+    publicationDate,
+    img,
+    description,
+  });
+
+  sendResponse(res, 200, true, book, null, "Create new book successful");
+});
+
+//Get all books
+
+bookController.getAllBooks = catchAsync(async (req, res, next) => {
+    const { page = 1, limit = 10, search, minPrice, maxPrice } = req.query;
+  
+    const pageNumber = parseInt(page);
+    const limitNumber = parseInt(limit);
+  
+    const skip = (pageNumber - 1) * limitNumber;
+  
+    const searchQuery = {
+      isDeleted: false,
+    };
+  
+    if (search) {
+      const yearPattern = /\b\d{4}\b/;
+      const yearMatch = search.match(yearPattern);
+  
+      if (yearMatch) {
+        searchQuery.publicationDate = {
+          $regex: new RegExp(`\\b${yearMatch[0]}\\b`),
+        };
+      } else {
+        searchQuery.$or = [
+          { name: { $regex: new RegExp(search, "i") } },
+          { categories: { $regex: new RegExp(search, "i") } },
+          { author: { $regex: new RegExp(search, "i") } },
+          { publicationDate: { $regex: new RegExp(search, "i") } },
+        ];
+      }
+    }
+  
+    if (minPrice && maxPrice) {
+      const minPriceValue = parseFloat(minPrice);
+      const maxPriceValue = parseFloat(maxPrice);
+      searchQuery.price = { $gte: minPriceValue, $lte: maxPriceValue };
+    }
+  
+    const result = await Book.aggregate([
+      {
+        $match: searchQuery,
+      },
+      {
+        $lookup: {
+          from: "bookcategories",
+          let: { bookId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: { $eq: ["$$bookId", "$bookId"] },
+              },
+            },
+            {
+              $lookup: {
+                from: "categories",
+                localField: "categoryId",
+                foreignField: "_id",
+                as: "category",
+              },
+            },
+            {
+              $unwind: "$category",
+            },
+            {
+              $project: {
+                _id: 0,
+                categoryName: "$category.categoryName",
+              },
+            },
+          ],
+          as: "categories",
+        },
+      },
+      {
+        $project: {
+          name: 1,
+          author: 1,
+          price: 1,
+          publicationDate: 1,
+          img: 1,
+          description: 1,
+          categories: "$categories.categoryName",
+        },
+      },
+      {
+        $facet: {
+          paginatedBooks: [{ $skip: skip }, { $limit: limitNumber }],
+          totalCount: [{ $count: "total" }],
+        },
+      },
+    ]);
+  
+    const { paginatedBooks, totalCount } = result[0];
+  
+    const totalPages =
+      paginatedBooks.length > 0
+        ? Math.ceil(totalCount[0].total / limitNumber)
+        : 0;
+  
+    const response = {
+      books: paginatedBooks,
+      totalPages,
+    };
+  
+    sendResponse(res, 200, true, response, null, "Books retrieved successfully");
+  });
+
+// Get book by id
+bookController.getlBookById = catchAsync(async (req, res, next) => {
+
+});
+
+// Search for books by name, author, genre, or other criteria
+bookController.searchBook = catchAsync(async (req, res, next) => {});
+
+//Filter books by various criteria
+bookController.filterBook = catchAsync(async (req, res, next) => {});
+
+//Update a book
+bookController.updateBook = catchAsync(async (req, res, next) => {});
+
+//Create or update discounted information for book
+bookController.discountBook = catchAsync(async (req, res, next) => {});
+
+//Update discounted information of a book
+bookController.updateDiscountBookById = catchAsync(
+  async (req, res, next) => {}
+);
+
+//Delete discounted information of a book
+bookController.deleteDiscountBook = catchAsync(async (req, res, next) => {});
+
+//Get all discounted books
+bookController.getDiscountBook = catchAsync(async (req, res, next) => {});
+
+//Delete a book
+bookController.deleteBook = catchAsync(async (req, res, next) => {});
 
 module.exports = bookController;
--- routes/book.api.js
+++ routes/book.api.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bookController = require("../controllers/book.controller");
+const authentication = require("../middlewares/authentication");
 
 /**
  * @route POST /books/
@@ -7,6 +9,13 @@ const router = express.Router();
  * @body { name, author, price, publicationDate }
  * @access admin
  */
+router.post(
+    '/',
+    authentication.loginRequired,
+    authentication.authorize(["admin"]),
+    bookController.createBook
+  );
+  
 
 /**
  * @route GET /books
@@ -14,6 +23,12 @@ const router = express.Router();
  * @body none
  * @access Public
  */
+router.get(
+    '/',
+    authentication.loginRequired,
+    bookController.getAllBooks
+  );
+
 
 /**
  * @route GET /books/:id
@@ -22,12 +37,23 @@ const router = express.Router();
  * @access Public
  */
 
+router.post(
+    '/:id',
+    authentication.loginRequired,
+    bookController.getlBookById
+  );
+
 /**
  * @route GET /books/search
  * @description Search for books by name, author, genre, or other criteria
  * @query { name, author, genre, priceRange, rating }
  * @access Public
  */
+router.get(
+    '/search',
+    authentication.loginRequired,
+    bookController.searchBook
+  );
 
 /**
  * @route GET /books/filter
@@ -35,6 +61,12 @@ const router = express.Router();
  * @query { genre, minPrice, maxPrice, minRating }
  * @access Public
  */
+router.get(
+    '/search',
+    authentication.loginRequired,
+    bookController.filterBook
+  );
+
 
 /**
  * @route PUT /books/:id
@@ -43,12 +75,26 @@ const router = express.Router();
  * @access admin
  */
 
+router.put(
+    '/:id',
+    authentication.loginRequired,
+    authentication.authorize(["admin"]),
+    bookController.createBook
+  );
+
 /**
  * @route POST /books/discount
  * @description Create or update discounted information for a book
  * @body { bookId, discountRate, discountedPrice }
  * @access Admin
  */
+
+router.post(
+    '/discount',
+    authentication.loginRequired,
+    authentication.authorize(["admin"]),
+    bookController.discountBook
+  );
 
 /**
  * @route PUT /books/discount/:id
@@ -57,12 +103,26 @@ const router = express.Router();
  * @access Admin
  */
 
+router.put(
+    '/discount/:id',
+    authentication.loginRequired,
+    authentication.authorize(["admin"]),
+    bookController.discountBook
+  );
+
 /**
  * @route DELETE /books/discount/:id
  * @description Delete discounted information of a book
  * @body none
  * @access Admin
  */
+
+router.delete(
+    '/discount/:id',
+    authentication.loginRequired,
+    authentication.authorize(["admin"]),
+    bookController.deleteBook
+  );
 
 /**
  * @route GET /books/discount
@@ -71,11 +131,25 @@ const router = express.Router();
  * @access Public
  */
 
+router.get(
+    '/discount',
+    authentication.loginRequired,
+    authentication.authorize(["admin"]),
+    bookController.getDiscountBook
+  );
+
 /**
  * @route DELETE /books/:id
  * @description Delete a book
  * @body none
  * @access admin
  */
+
+router.delete(
+    '/:id',
+    authentication.loginRequired,
+    authentication.authorize(["admin"]),
+    bookController.deleteBook
+  );
 
 module.exports = router
